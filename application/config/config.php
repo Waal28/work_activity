034643aefb2c9abff19c686f24da0a5d730bc19e
IwaL FaizuL <iwalfaizul@gmail.com>
2025-06-02 21:57:59 +0700

    chore: tambah keterangan pada default controller dan default root page url

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/work_activity/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = ''; // hilangkan index.php dari url (gunakan .htaccess)
 
 /*
 |--------------------------------------------------------------------------
