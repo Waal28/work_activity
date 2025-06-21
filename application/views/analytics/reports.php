868ac2d27f8d535e5eb4b2c51fa4803a3cfaa28c
IwaL FaizuL <iwalfaizul@gmail.com>
2025-06-21 22:28:19 +0700

    fix: tambah perkondisian jika pekerjaan masih kosong dan yang login bukan staf

--- application/views/analytics/reports.php
+++ application/views/analytics/reports.php
@@ -34,42 +34,49 @@
             $total_bobot_kpi += $item['bobot'];
         }
       ?>
-      <?php foreach ($rows['pekerjaan'] as $index => $item): ?>
-        <?php
-          $realisasi = $item['target_semester_1'] + $item['target_semester_2'];
-          $performance = hitung_performance($realisasi, $item['annual_target']);
-          $weighted_performance = hitung_weighted_performance($performance, $item['bobot'], $total_bobot_kpi);
-          $weighted_performance_kpi += $weighted_performance;
-        ?>
-        <?php if ($index === 0): ?>
-          <tr>
-            <td class="bg-primary text-white fw-bold" rowspan="<?= count($rows['pekerjaan']) ?>">Cascading<br>KPI Fungsi</td>
-            <td><?= $item['judul'] ?></td>
-            <td><?= $item['freq_mon'] ?></td>
-            <td><?= rtrim(rtrim($item['bobot'], '0'), '.') ?>%</td>
-            <td><?= $item['satuan'] ?></td>
-            <td><?= rtrim(rtrim($item['annual_target'], '0'), '.') ?>%</td>
-            <td><?= rtrim(rtrim($item['target_semester_1'], '0'), '.') ?>%</td>
-            <td><?= rtrim(rtrim($item['target_semester_2'], '0'), '.') ?>%</td>
-            <td><?= $realisasi ?>%</td>
-            <td><?= $performance ?>%</td>
-            <td><?= $weighted_performance ?>%</td>
-          </tr>
-        <?php else: ?>
-          <tr>
-            <td><?= $item['judul'] ?></td>
-            <td><?= $item['freq_mon'] ?></td>
-            <td><?= rtrim(rtrim($item['bobot'], '0'), '.') ?>%</td>
-            <td><?= $item['satuan'] ?></td>
-            <td><?= rtrim(rtrim($item['annual_target'], '0'), '.') ?>%</td>
-            <td><?= rtrim(rtrim($item['target_semester_1'], '0'), '.') ?>%</td>
-            <td><?= rtrim(rtrim($item['target_semester_2'], '0'), '.') ?>%</td>
-            <td><?= $realisasi ?>%</td>
-            <td><?= $performance ?>%</td>
-            <td><?= $weighted_performance ?>%</td>
-          </tr>
-        <?php endif; ?>
-      <?php endforeach; ?>
+      <?php if (empty($rows['pekerjaan'])): ?>
+        <tr>
+          <td class="bg-primary text-white fw-bold text-capitalize">cascading<br>kpi fungsi</td>
+          <td class="text-center" colspan="11">Pekerjaan masih kosong</td>
+        </tr>
+      <?php else: ?>
+        <?php foreach ($rows['pekerjaan'] as $index => $item): ?>
+          <?php
+            $realisasi = $item['target_semester_1'] + $item['target_semester_2'];
+            $performance = hitung_performance($realisasi, $item['annual_target']);
+            $weighted_performance = hitung_weighted_performance($performance, $item['bobot'], $total_bobot_kpi);
+            $weighted_performance_kpi += $weighted_performance;
+          ?>
+          <?php if ($index === 0): ?>
+            <tr>
+              <td class="bg-primary text-white fw-bold text-capitalize" rowspan="<?= count($rows['pekerjaan']) ?>">cascading<br>kpi fungsi</td>
+              <td><?= $item['judul'] ?></td>
+              <td><?= $item['freq_mon'] ?></td>
+              <td><?= rtrim(rtrim($item['bobot'], '0'), '.') ?>%</td>
+              <td><?= $item['satuan'] ?></td>
+              <td><?= rtrim(rtrim($item['annual_target'], '0'), '.') ?>%</td>
+              <td><?= rtrim(rtrim($item['target_semester_1'], '0'), '.') ?>%</td>
+              <td><?= rtrim(rtrim($item['target_semester_2'], '0'), '.') ?>%</td>
+              <td><?= $realisasi ?>%</td>
+              <td><?= $performance ?>%</td>
+              <td><?= $weighted_performance ?>%</td>
+            </tr>
+          <?php else: ?>
+            <tr>
+              <td><?= $item['judul'] ?></td>
+              <td><?= $item['freq_mon'] ?></td>
+              <td><?= rtrim(rtrim($item['bobot'], '0'), '.') ?>%</td>
+              <td><?= $item['satuan'] ?></td>
+              <td><?= rtrim(rtrim($item['annual_target'], '0'), '.') ?>%</td>
+              <td><?= rtrim(rtrim($item['target_semester_1'], '0'), '.') ?>%</td>
+              <td><?= rtrim(rtrim($item['target_semester_2'], '0'), '.') ?>%</td>
+              <td><?= $realisasi ?>%</td>
+              <td><?= $performance ?>%</td>
+              <td><?= $weighted_performance ?>%</td>
+            </tr>
+          <?php endif; ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
   
       <!-- Objectives -->
       <?php
@@ -78,27 +85,29 @@
             $total_bobot_obj += $item['bobot'];
         }
       ?>
-      <?php foreach ($rows['objectives'] as $objective): ?>
-        <?php
-          $realisasi = $objective['target_semester_1'] + $objective['target_semester_2'];
-          $performance = $objective['hse_point'] + $objective['dev_point'] + $objective['community_point'];
-          $weighted_performance = hitung_weighted_performance($performance, $objective['bobot'], $total_bobot_obj);
-          $weighted_performance_obj += $weighted_performance;
-        ?>
-        <tr>
-          <td class="bg-primary text-white fw-bold"><?= $objective['nama_objective'] ?></td>
-          <td><?= $objective['deskripsi'] ?></td>
-          <td><?= $objective['freq_mon'] ?></td>
-          <td><?= rtrim(rtrim($objective['bobot'], '0'), '.') ?>%</td>
-          <td><?= $objective['satuan'] ?></td>
-          <td><?= rtrim(rtrim($objective['annual_target'], '0'), '.') ?>%</td>
-          <td><?= rtrim(rtrim($objective['target_semester_1'], '0'), '.') ?>%</td>
-          <td><?= rtrim(rtrim($objective['target_semester_2'], '0'), '.') ?>%</td>
-          <td><?= $realisasi ?>%</td>
-          <td><?= $performance ?>%</td>
-          <td><?= $weighted_performance ?>%</td>
-        </tr>
-      <?php endforeach; ?>
+      <?php if ($this->session->userdata('role') === 'Staf'): ?>
+        <?php foreach ($rows['objectives'] as $objective): ?>
+          <?php
+            $realisasi = $objective['target_semester_1'] + $objective['target_semester_2'];
+            $performance = $objective['hse_point'] + $objective['dev_point'] + $objective['community_point'];
+            $weighted_performance = hitung_weighted_performance($performance, $objective['bobot'], $total_bobot_obj);
+            $weighted_performance_obj += $weighted_performance;
+          ?>
+          <tr>
+            <td class="bg-primary text-white fw-bold"><?= $objective['nama_objective'] ?></td>
+            <td><?= $objective['deskripsi'] ?></td>
+            <td><?= $objective['freq_mon'] ?></td>
+            <td><?= rtrim(rtrim($objective['bobot'], '0'), '.') ?>%</td>
+            <td><?= $objective['satuan'] ?></td>
+            <td><?= rtrim(rtrim($objective['annual_target'], '0'), '.') ?>%</td>
+            <td><?= rtrim(rtrim($objective['target_semester_1'], '0'), '.') ?>%</td>
+            <td><?= rtrim(rtrim($objective['target_semester_2'], '0'), '.') ?>%</td>
+            <td><?= $realisasi ?>%</td>
+            <td><?= $performance ?>%</td>
+            <td><?= $weighted_performance ?>%</td>
+          </tr>
+        <?php endforeach; ?>
+      <?php endif; ?>
         <tr>
           <td colspan="10"></td>
           <td class="bg-primary text-white fw-bold">
