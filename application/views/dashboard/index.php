96634ee58547d243a06c58c5a83466aa5e49ee1d
keyziacnst <rosalinkeyziaconstantia@gmail.com>
2025-07-28 00:29:32 +0700

    Update index.php

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -15,11 +15,6 @@ $deg_small_blue = ($metrik['rata_skor'] / 100) * 360;
 $deg_small_gray = ($metrik['rata_progress'] / 100) * 360;
 
 ?>
-<?=
-'<pre>';
-print_r($this->session->userdata());
-'</pre>';
-?>
 <style>
     .stats-card {
         background: white;
